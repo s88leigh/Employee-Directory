d05b6d16fe8fe264c929aae2211e7bcb0e5a5995
Sophie <sophie88leigh@gmail.com>
2020-02-12 13:16:10 -0500

    add style to Table

--- employee-directory-react/src/components/TableArea/index.js
+++ employee-directory-react/src/components/TableArea/index.js
@@ -1,51 +1,52 @@
 import React from 'react';
+import './tableStyle.css';
 
 export default class TableArea extends React.Component {
 
     render() {
-        return(
+        return (
             <div>
-               <table>
-  <tr>
-    <th>Image</th>
-    <th>Name</th>
-    <th>Phone</th>
-    <th>Email</th>
-    <th>DOB</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-    <td>Germany</td> 
-    <td>Germany</td>
+                <table>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Email</th>
+                        <th>DOB</th>
+                    </tr>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
 
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-    <td>Germany</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-    <td>Germany</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-    <td>Germany</td>
-    <td>Germany</td>
-  </tr>
-  </table>
-                TableArea
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td>Ernst Handel</td>
+                        <td>Roland Mendel</td>
+                        <td>Austria</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td>Island Trading</td>
+                        <td>Helen Bennett</td>
+                        <td>UK</td>
+                        <td>Germany</td>
+                        <td>Germany</td>
+                    </tr>
+                </table>
+
             </div>
         )
     }
-    
+
 }
