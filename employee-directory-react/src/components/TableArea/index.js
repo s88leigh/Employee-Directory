3cbb89800fc4f8f09b095bc5e670ddee3ac62b9a
Sophie <sophie88leigh@gmail.com>
2020-02-12 13:12:13 -0500

    added table

--- employee-directory-react/src/components/TableArea/index.js
+++ employee-directory-react/src/components/TableArea/index.js
@@ -5,6 +5,44 @@ export default class TableArea extends React.Component {
     render() {
         return(
             <div>
+               <table>
+  <tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Phone</th>
+    <th>Email</th>
+    <th>DOB</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+    <td>Germany</td> 
+    <td>Germany</td>
+
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+    <td>Germany</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+    <td>Germany</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+    <td>Germany</td>
+    <td>Germany</td>
+  </tr>
+  </table>
                 TableArea
             </div>
         )
