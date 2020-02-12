2406276a665f294593438f9b27f47a9db51c7897
Sophie <sophie88leigh@gmail.com>
2020-02-12 11:15:46 -0500

    added style to pageheader and search section

--- employee-directory-react/src/App.js
+++ employee-directory-react/src/App.js
@@ -6,14 +6,31 @@ import TableHeader from './components/TableHeader';
 import TableData from './components/TableData';
 
 
+import './App.css';
+
+
 function App() {
   return (
     <div>
+    <div>
     <PageHeader/>
+    </div>
+
+    <div>
     <SearchBar/>
+    </div>
+
+    <div>
     <TableArea/>
+    </div>
+
+    <div>
     <TableHeader/>
+    </div>
+
+    <div>
     <TableData/>
+    </div>
     </div>
   );
 }
--- employee-directory-react/src/components/SearchBar/index.js
+++ employee-directory-react/src/components/SearchBar/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './search-style.css';
+
 
 export default class SearchBar extends React.Component {
+
 
     state = {
         search: "", //we are searching for...
@@ -18,7 +21,7 @@ export default class SearchBar extends React.Component {
 
     render() { //include some states. we're gonna change come states that are put in
         return(
-            <div>
+            <div className='row'>
                 <form>
                     <input
                     handleInputChange={this.handleInputChange}
